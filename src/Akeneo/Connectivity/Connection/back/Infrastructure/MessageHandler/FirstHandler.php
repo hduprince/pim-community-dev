da5ab2b776367f6957bfee403cc15116e8834def
Paul Chasle <paul.chasle@akeneo.com>
2020-07-31 19:00:25 +0200

    f

---
+++ src/Akeneo/Connectivity/Connection/back/Infrastructure/MessageHandler/FirstHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Connectivity\Connection\Infrastructure\MessageHandler;
+
+use Akeneo\Pim\Enrichment\Bundle\Message\ProductUpdated;
+use Symfony\Component\Messenger\Handler\MessageSubscriberInterface;
+
+class FirstHandler implements MessageSubscriberInterface
+{
+    public static function getHandledMessages(): iterable
+    {
+        yield ProductUpdated::class => [];
+    }
+
+    public function __invoke(ProductUpdated $message)
+    {
+        echo "FIRST\t" . $message->getContent() . PHP_EOL;
+    }
+}
---
+++ src/Akeneo/Pim/Enrichment/Bundle/Message/ProductUpdated.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\Pim\Enrichment\Bundle\Message;
+
+/**
+ * @copyright 202O Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ProductUpdated implements BusinessEvent
+{
+    private $content;
+
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+}
